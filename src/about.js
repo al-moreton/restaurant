1d46a624f4e4a65140080b0fad3748b0180faca4
al-moreton <moreton.alexander@gmail.com>
2025-10-26 21:43:56 +0000

    Deployment commit

--- src/about.js
+++
@@ -1,9 +0,0 @@
-const aboutContainer = document.createElement('div');
-aboutContainer.classList.add('about-container');
-
-const title = document.createElement('h1');
-title.textContent = 'About us';
-
-aboutContainer.appendChild(title);
-
-export { aboutContainer };
--- src/menu.js
+++
@@ -1,9 +0,0 @@
-const menuContainer = document.createElement('div');
-menuContainer.classList.add('menu-container');
-
-const title = document.createElement('h1');
-title.textContent = 'Menu';
-
-menuContainer.appendChild(title);
-
-export { menuContainer };
